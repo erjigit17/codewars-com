091d414e99bfa81b8d87e109c0a8177a8bb4e3a9
Эржигит Имамов <erjigit17@gmail.com>
2021-09-04 22:54:30 +0600

    add shortes and elegance solytion

--- findOutlier.js
+++ findOutlier.js
@@ -4,34 +4,42 @@
 // // Test.assertEquals(findOutlier([0,0,3,0,0]), 3)
 // // Test.assertEquals(findOutlier([1,1,0,1,1]), 0)
 
-// arr.legth >= 3
-
+// arr.legth >= 3, 
+// My fastest 
 function findOutlier(integers){
   let odd = 0
   let even = 0
   for (let i = 0; i < 3; i++){
-    (integers[i] % 2 === 0 || integers[i] % 2 === -0) ? even ++ : odd++
+    (integers[i] % 2 === 0 || -0) ? even ++ : odd++
   }
-
-  for (item of integers) {
-    if (!(item % 2 === 0 || item % 2 === -0) && even >= 2) {
-      return item
-    }
-    if ((item % 2 === 0 || item % 2 === -0) && odd >= 2) {
-      return item
-    }
-  }
+  if (even >= 2){
+    for (item of integers) {
+      if (!(item % 2 === 0 || -0)) {
+        return item
+      }}} else {
+    for (item of integers) {
+      if ((item % 2 === 0 || -0)) {
+        return item
+  }}}
 }
 
 
-const a = [1, 2, 3]
-console.log(findOutlier(a))
 
-//  shirtes
-// function findOutlier(int){
-//   var even = int.filter(a=>a%2==0);
-//   var odd = int.filter(a=>a%2!==0);
-//   return even.length==1? even[0] : odd[0];
-// }
+// the shortest
+function findOutlier(int){
+  var even = int.filter(a=>a%2==0);
+  var odd = int.filter(a=>a%2!==0);
+  return even.length==1? even[0] : odd[0];
+}
 
+// elegance
+function findOutlier(integers){
+  return integers.slice(0,3).filter(even).length >=2 ? integers.find(odd) : integers.find(even);
+}
+function even(num){
+  return (num % 2 == 0);
+}
+function odd(num){
+  return !even(num)
+}
 

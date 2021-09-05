dc39269844b02f2ea62f4bc43083b65350fdc3e9
Эржигит Имамов <erjigit17@gmail.com>
2021-09-05 09:42:49 +0600

    open solutions

--- squareDigits.js
+++ squareDigits.js
@@ -11,26 +11,26 @@ function squareDigits(num){
 
 
 // #2 Time: 768ms (es6)
-// function squareDigits(num){ 
-//   const inStr = num.toString()
-//   let result = ''
-//   for (let item of inStr) {
-//     const curentNum = parseInt(item) 
-//     result += (curentNum ** 2).toString()
-//   }
-//   return parseInt(result);
-// }
+function squareDigits(num){ 
+  const inStr = num.toString()
+  let result = ''
+  for (let item of inStr) {
+    const curentNum = parseInt(item) 
+    result += (curentNum ** 2).toString()
+  }
+  return parseInt(result);
+}
 
 // 791ms
-// function squareDigits(num){ 
-//   const inStr = num.toString()
-//   let result = '' 
-//   inStr.split('').forEach(item => {
-//     const curentNum = parseInt(item) 
-//     result += (curentNum ** 2).toString()
-//   })  
-//   return parseInt(result)
-// }
+function squareDigits(num){ 
+  const inStr = num.toString()
+  let result = '' 
+  inStr.split('').forEach(item => {
+    const curentNum = parseInt(item) 
+    result += (curentNum ** 2).toString()
+  })  
+  return parseInt(result)
+}
 
   
 console.log(squareDigits(12))
